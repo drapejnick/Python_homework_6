e1486a5810fc0ed92b632d2399670d6adc0a7fb1
Vladimir <drapejnick@gmail.com>
2023-01-26 10:31:51 +0100

    add exercersises

---
+++ 032.py
@@ -0,0 +1,19 @@
+# Задача 32: Определить элементs массива (списка), значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+# Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# 1
+# 19
+# Вывод: [1, 9, 13, 14, 19]
+
+import random
+mas = [random.randint(-50, 50) for i in range(random.randint(10, 20))]
+print(*mas)
+maxi = int(input("MAX= "))
+mini = int(input("MIN= "))
+masi = []
+if maxi >= mini:
+    for i in range(len(mas)):
+        if maxi >= mas[i] and mini <= mas[i]:
+            masi.append(i)
+    print("Кол-во:", len(masi))
+    print("Индексы:", masi)
